4a932346594ec7951455cb9c0e483f66a152b55c
Alexandre Gava <alexandre.log@hotmail.com>
2021-01-07 20:55:20 -0300

    Correcao de bug 2

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from insertDB import insertUsuario
 app = Flask("ApiComFlask")
 
 #testando versionamento
-#correcao de bug
+#correcao de bug 2
 @app.route("/cadastra/usuario", methods=["POST"])
 def cadastraUsuario():
 
